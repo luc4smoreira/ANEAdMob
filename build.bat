e3ec9615b8213af47e95251b5bc54cdb5ea7e7db
Lucas Miranda <lucas@hastatus.com.br>
2020-08-04 11:12:19 -0300

    Removing old batch files

--- build.bat
+++
@@ -1,5 +0,0 @@
-
-SET PLATFORM_ANDROID= -platform Android-ARM -C android\AdMob\build\ .
-SET PLATFORM_DEFAULT= -platform default -C default\ .
-
-"%AIR_HOME%\bin\adt.bat" -package -target ane AdMob.ane air\extension.xml -swc air/AdMob/bin/AdMob.swc %PLATFORM_ANDROID% %PLATFORM_DEFAULT%
--- build64.bat
+++
@@ -1,4 +0,0 @@
-SET PLATFORM_ANDROID= -platform Android-ARM64 -C android\AdMob\build\ .
-SET PLATFORM_DEFAULT= -platform default -C default\ .
-
-"%AIR_HOME%\bin\adt.bat" -package -target ane AdMob64.ane air\extension64.xml -swc air/AdMob/bin/AdMob.swc %PLATFORM_ANDROID% %PLATFORM_DEFAULT%
